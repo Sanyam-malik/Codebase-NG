f174fad8dc7f2988d81e90d3b4004a2c8dfc4ce6
Sam Arora <samarora1051@gmail.com>
2024-03-05 16:49:48 +0530

    Removed Console.log

--- src/app/codebase.service.ts
+++ src/app/codebase.service.ts
@@ -97,7 +97,6 @@ export class CodebaseService {
 
   getCompany(name: string | undefined) {
     if(name) {
-      console.log(this.companies);
       const list = this.companies.filter(item => item.name.toLowerCase() == name);
       return list[0];
     } else {
