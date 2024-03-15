8c6ba2552b9f8aa245facdd26ba207ad0806aca1
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-16 04:31:14 +0530

    Added Level Component

--- src/app/problems/level/level.component.ts
+++ src/app/problems/level/level.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../problem';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CodebaseService } from '../../codebase.service';
 
 @Component({
   selector: 'app-level',
   templateUrl: './level.component.html',
   styleUrl: './level.component.scss'
 })
-export class LevelComponent {
+export class LevelComponent implements OnInit {
+  filter: any | undefined = undefined;
+  breadcrumb: any[] = [
+    {
+      name: 'Level',
+      url: `/problem/level`
+    }
+  ];
 
+  data : Problem[] = [];
+
+  get level(){
+    const slug = this.route.snapshot.paramMap.get('level');
+    if(slug) {
+      return this.codebase.getLevel(slug);
+    } else {
+      return undefined;
+    }
+  }
+
+
+  constructor(private codebase: CodebaseService, private router: Router, private route: ActivatedRoute) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
+
+  ngOnInit(): void {
+    this.data = this.route.snapshot.data['apiResponse']['problems'];
+    this.breadcrumb.push(
+      {
+        name: this.level ? this.level?.level : '',
+        url: `/problem/level/${this.level?.slug}`
+      }
+    )
+    this.filter = {
+      key: 'level',
+      value: this.level?.level
+    }
+  }
 }
