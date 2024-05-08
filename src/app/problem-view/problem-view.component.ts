e5ae24ff4ebb7226cd8e976a6e269fcd3594a09a
Sam Arora <samarora1051@gmail.com>
2024-05-08 07:55:17 +0530

    Added New Copy Button

--- src/app/problem-view/problem-view.component.ts
+++ src/app/problem-view/problem-view.component.ts
@@ -67,7 +67,21 @@ export class ProblemViewComponent {
     }
 
     onCopyToClipboard(text: string) {
-        window.navigator.clipboard.writeText(text);
+        if(!window.navigator || !window.navigator.clipboard) {
+            const selBox = document.createElement('textarea');
+            selBox.style.position = 'fixed';
+            selBox.style.left = '0';
+            selBox.style.top = '0';
+            selBox.style.opacity = '0';
+            selBox.value = text;
+            document.body.appendChild(selBox);
+            selBox.focus();
+            selBox.select();
+            document.execCommand('copy');
+            document.body.removeChild(selBox);
+        } else {
+            window.navigator.clipboard.writeText(text);
+        }
         this.message.success("Code Copied to Clipboard...");
     }
 }
