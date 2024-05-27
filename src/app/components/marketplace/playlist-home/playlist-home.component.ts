aeb2270a8a3bdaaa9c6effe6df3c854022be407e
Sam Arora <samarora1051@gmail.com>
2024-05-28 01:59:27 +0530

    MISC: Added Working For Sheets and Playlists

--- src/app/components/marketplace/playlist-home/playlist-home.component.ts
+++ src/app/components/marketplace/playlist-home/playlist-home.component.ts
@@ -11,14 +11,50 @@ import { PlaylistHome } from '../../../data-models/playlist-marketplace';
 })
 export class PlaylistHomeComponent implements OnInit {
 
+  search: string = "";
   playlists: PlaylistHome[] = [];
+  pageIndex: number = 1;
+  pageSize = 16;
+  pageTotal = 0;
+  paginatedRecords: PlaylistHome[][] = [];
   
   constructor(private http: HttpClient, private route: ActivatedRoute) {
-    this.playlists = this.route.snapshot.data['apiResponse']['playlist'];
+    this.playlists = this.route.snapshot.data['apiResponse']['playlists'];
   }
 
   ngOnInit() {
-    
+    this.paginatedRecords = this.generateGrids(this.playlists); 
+  }
+
+  initSearch() {
+    if(this.search) {
+      const filteredRecords = this.playlists.filter(item => 
+        item.name.toLowerCase().includes(this.search.toLowerCase())
+      );
+      this.paginatedRecords = this.generateGrids(filteredRecords);
+    } else {
+      this.paginatedRecords = this.generateGrids(this.playlists); 
+    }
+    this.pageIndex = 1;
+  }
+
+  generateGrids(records: PlaylistHome[]): PlaylistHome[][] {
+    this.pageTotal = records.length;
+    const startIndex = (this.pageIndex - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    const pageRecords = records.slice(startIndex, endIndex);
+    const grids: PlaylistHome[][] = [];
+
+    for (let i = 0; i < pageRecords.length; i += 4) {
+      grids.push(pageRecords.slice(i, i + 4));
+    }
+
+    return grids;
+  }
+
+  pageChange(pageNo: number) {
+    this.pageIndex = pageNo;
+    this.paginatedRecords = this.generateGrids(this.playlists); 
   }
 
 }
--- src/app/components/marketplace/sheet-home/sheet-home.component.ts
+++ src/app/components/marketplace/sheet-home/sheet-home.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 import { SheetHome } from '../../../data-models/sheet-marketplace';
 
@@ -10,14 +9,50 @@ import { SheetHome } from '../../../data-models/sheet-marketplace';
   styleUrl: './sheet-home.component.scss'
 })
 export class SheetHomeComponent implements OnInit {
-
+  
+  search: string = "";
   sheets: SheetHome[] = [];
+  pageIndex: number = 1;
+  pageSize = 16;
+  pageTotal = 0;
+  paginatedRecords: SheetHome[][] = [];
   
   constructor(private http: HttpClient, private route: ActivatedRoute) {
-    this.sheets = this.route.snapshot.data['apiResponse']['sheet'];
+    this.sheets = this.route.snapshot.data['apiResponse']['sheets'];
   }
 
   ngOnInit() {
-    
+    this.paginatedRecords = this.generateGrids(this.sheets); 
+  }
+
+  initSearch() {
+    if(this.search) {
+      const filteredRecords = this.sheets.filter(item => 
+        item.name.toLowerCase().includes(this.search.toLowerCase())
+      );
+      this.paginatedRecords = this.generateGrids(filteredRecords);
+    } else {
+      this.paginatedRecords = this.generateGrids(this.sheets); 
+    }
+    this.pageIndex = 1;
+  }
+
+  generateGrids(records: SheetHome[]): SheetHome[][] {
+    this.pageTotal = records.length;
+    const startIndex = (this.pageIndex - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    const pageRecords = records.slice(startIndex, endIndex);
+    const grids: SheetHome[][] = [];
+
+    for (let i = 0; i < pageRecords.length; i += 4) {
+      grids.push(pageRecords.slice(i, i + 4));
+    }
+
+    return grids;
+  }
+
+  pageChange(pageNo: number) {
+    this.pageIndex = pageNo;
+    this.paginatedRecords = this.generateGrids(this.sheets); 
   }
 }
