0cff4462a6320b2768c00e4cee377af418399c6b
Sam Arora <samarora1051@gmail.com>
2024-10-17 00:19:59 +0530

    MISC: Added New Test Case Structure

--- src/app/data-models/testcase.ts
+++ src/app/data-models/testcase.ts
@@ -2,13 +2,25 @@ import { Problem } from "./problem";
 import { Item } from "./sheet";
 
 export interface TestCase {
-    id: number;
+    id: string,
     problem?: Problem | null,
     sheet_item?: Item | null
     call_function: string,
-    call_inputs: string[],
-    call_input_type: string,
-    required_output_type: string,
-    required_outputs: string[],
+    call_inputs: TestInputs[],
+    required_outputs: TestOutputs[],
     time_limit: number
+}
+
+export interface TestInputs {
+    id: string,
+    type: string,
+    subtype: string,
+    values: any[]
+}
+
+export interface TestOutputs {
+    id: string,
+    type: string,
+    subtype: string,
+    values: any[]
 }
